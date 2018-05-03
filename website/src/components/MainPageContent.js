0cc741b3f7fc435d325dbe5d07862203cf377fd4
isaacJChen <isaacjc@uw.edu>
2018-05-02 18:31:48 -0700

    add benifits and content

--- website/src/components/MainPageContent.js
+++ website/src/components/MainPageContent.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import Bio from './Bio'
+import ScreenShots from './ScreenShots'
 import Skills from './Skills'
+import pie from '../img/pie.png'
+import bar from '../img/bar.png'
 
 export default class MainPageContent extends React.Component{
   render() {
@@ -9,6 +12,11 @@ export default class MainPageContent extends React.Component{
       <div>
         <Bio/>
         <Skills/>
+        <center>
+          <img src={pie} alt="pie chart"/>
+          <img src={bar} alt="bar chart"/>
+        </center>
+        <ScreenShots/>
       </div>
     )
   }
---
+++ website/src/components/ScreenShots.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class ScreenShots extends React.Component{
+  render() {
+    return(
+      <div className="p-5 projects">
+        <h3 className="text-white">Solution</h3>
+        <p className="text-white">Tunite attempts to provide musicians with a tool that promotes new collaborations as well as highlight collaborations that are done well. By using Tunite, artists can share new music that has the potential to influence, motivate, or inspire the next, biggest hit. Music posted on this platform becomes open to the public for collaboration and the best collaborations become celebrated by the community.</p>
+        <p className="text-white">Collaborations are created by taking an existing song and sampling it, mixing it, or adding to it. New collaborations are easy to seek using the explore feature. When an artists posts a song, they can select a fifteen second snippet that advertises their version of a song. Then all collaborations from an original song become explorable by sifting through each songs’ snippet. Intriguing songs can be saved to listen to later.</p>
+        <h3 className="text-white">Benefit</h3>
+        <p className="text-white">This tool eliminates one of the barriers of entry into the music marketplace: barrier to creating meaningful connections. By using this app, musicians will be able to create new connections simply by becoming inspired to create music from what they have listened to on the platform.</p>
+        <p className="text-white">With this tool, no one will need to rely on word-of-mouth to create meaningful connections.  All you need is the passion to pursue music.</p>
+      </div>
+    )
+  }
+}
